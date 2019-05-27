c9c980091e7c0c4dd3a6a894bdebfeffa929e80f
Brian Salemi <briandsalemi@gmail.com>
2019-05-27 23:39:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   def self.new_by_filename(file)
     strings = file.split(" - ")
     song = Song.new(strings[1])
-    song.artist.name = strings[1]
+    song.artist = strings[1]
     binding.pry
   end
 
