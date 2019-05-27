f84ff4cd0a74b6767943c18f31a6678543e50d66
Brian Salemi <briandsalemi@gmail.com>
2019-05-27 23:44:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   def self.new_by_filename(file)
     strings = file.split(" - ")
     song = Song.new(strings[1])
-    artist = Artist.find_or_create_by_name(artist_name)
+    artist = Artist.find_or_create_by_name(artist)
     artist.add_song(song)
   end
 
